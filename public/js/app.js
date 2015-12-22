aaa7b8a66f0d5390b16d2ef9c19bc056e6ce21b1
Shannon <smwhelchel@gmail.com>
2015-12-22 13:19:03 -0800

    adding caption to menu items, #1

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,40 @@
-// var menu= document.getElementById('menu-nav');
-// menu.addEventListener('click', function() {
-  // fs.readFile ('menu.json', function loadJSON() {
-  //   var jsonFile='/menu.json';
-    var xhr= new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState==4) {
-        var menuObject = JSON.parse(xhr.responseText);
-        // document.getElementById("name").textContent;
-        // document.getElementById("price").textContent;
-        console.log(menuObject.drinks[0]);
-        for (i=0; i<menuObject.drinks.length; i++) {
-          var menuData = menuObject.drinks[i];
-          console.log(menuData);
-          var menuColumn= document.createElement('div');
-          menuColumn.setAttribute('class', 'col-sm-3 col-md-3 show-menu');
-          var thumbnail= document.createElement('div');
-          thumbnail.setAttribute('class', 'thumbnail');
-          var image= document.createElement('img');
-          var caption= document.createElement('div');
-          caption.setAttribute('class', 'caption');
-          caption.textContent= menuObject.drinks[i].name;
-          var button= document.createElement('button');
-          button.setAttribute('class', 'btn btn-default');
-          caption.appendChild(button);
-          image.appendChild(caption);
-          thumbnail.appendChild(image);
-          menuColumn.appendChild(thumbnail);
-          document.getElementById('menu-row').appendChild(menuColumn);
-        }
-      }
-    }; 
-    xhr.open('GET', '/js/menu.json', true)
-    xhr.send(null);
-    //console.log("file sent");
-//   });
-// };
+var myMenu= document.getElementById('menu-nav')
+var menuContainer= document.getElementById('menu-populate')
+myMenu.addEventListener('click', function() { menuContainer.className='show'})
+
+
+var xhr= new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState==4) {
+    var menuObject = JSON.parse(xhr.responseText);
+
+    console.log(menuObject.drinks[0]);
+    for (i=0; i<menuObject.drinks.length; i++) {
+      var menuData = menuObject.drinks[i];
+      console.log(menuData);
+      var menuColumn= document.createElement('div');
+      menuColumn.setAttribute('class', 'col-sm-3 col-md-3 show-menu');
+      var thumbnail= document.createElement('div');
+      thumbnail.setAttribute('class', 'thumbnail');
+      var image= document.createElement('img');
+      image.setAttribute('src', menuObject.drinks[i].image);
+      var caption= document.createElement('div');
+      caption.setAttribute('class', 'caption');
+      caption.textContent=menuObject.drinks[i].caption;
+      var hElement= document.createElement('h3');
+      hElement.textContent= menuObject.drinks[i].name;
+      var button= document.createElement('button');
+      button.setAttribute('class', 'btn btn-default menu-button');
+      hElement.appendChild(button);
+      caption.appendChild(hElement)
+      thumbnail.appendChild(image);
+      thumbnail.appendChild(caption);
+      menuColumn.appendChild(thumbnail);
+      document.getElementById('menu-row').appendChild(menuColumn);
+    }
+  }
+}; 
+xhr.open('GET', '/js/menu.json', true)
+xhr.send(null);
 
 
